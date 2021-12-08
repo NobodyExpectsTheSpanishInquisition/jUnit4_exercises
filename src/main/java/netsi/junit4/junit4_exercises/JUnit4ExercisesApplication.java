981822e67015fe7549d9b3fb19f7ba7ad195f66b
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2021-12-08 19:29:06 +0100

    init commit

---
+++ src/main/java/netsi/junit4/junit4_exercises/JUnit4ExercisesApplication.java
@@ -0,0 +1,13 @@
+package netsi.junit4.junit4_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit4ExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnit4ExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/netsi/junit4/junit4_exercises/JUnit4ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package netsi.junit4.junit4_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit4ExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
